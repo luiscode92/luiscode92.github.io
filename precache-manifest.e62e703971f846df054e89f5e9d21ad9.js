6f8a27f86a105ae906e3cd866dd0cc82e41003c0
Luis Sanjuan <luiscode92@gmail.com>
2020-11-20 02:53:11 -0500

    Updates

---
+++ precache-manifest.e62e703971f846df054e89f5e9d21ad9.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "7eead936977e763e4e129bb93430519e",
+    "url": "/static/media/prismlayout-img08.7eead936.png"
+  },
+  {
+    "revision": "ff4873124db10161ae95",
+    "url": "/static/css/main.31487beb.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b8b5761e71f82b454067",
+    "url": "/static/js/2.d63d6187.chunk.js"
+  },
+  {
+    "revision": "b5fa1b553516d1cf8d03d6b4588058a0",
+    "url": "/static/media/CircularStd-Black.b5fa1b55.woff"
+  },
+  {
+    "revision": "6f4fe3ca482221e7c0d8c1baa5f31100",
+    "url": "/static/media/CircularStd-Bold.6f4fe3ca.woff"
+  },
+  {
+    "revision": "cc06409e5a820f455b3b4883fbf9b72f",
+    "url": "/static/media/CircularStd-BookItalic.cc06409e.woff"
+  },
+  {
+    "revision": "e47ea4c0b9151fe6cbd8e2f332113559",
+    "url": "/static/media/CircularStd-Medium.e47ea4c0.woff"
+  },
+  {
+    "revision": "c47d7e581f2a563f8dedd4509c106fb8",
+    "url": "/static/media/CircularStd-Book.c47d7e58.woff"
+  },
+  {
+    "revision": "eddb70218f38749eaeeed7fba894fda8",
+    "url": "/static/media/CircularStd-MediumItalic.eddb7021.woff"
+  },
+  {
+    "revision": "2153a12d1db4eda5b954e54532175e5d",
+    "url": "/static/media/CircularStd-BlackItalic.2153a12d.woff"
+  },
+  {
+    "revision": "8d5dbe8149820b1202cbaf34b7204598",
+    "url": "/static/media/CircularStd-BoldItalic.8d5dbe81.woff"
+  },
+  {
+    "revision": "3c9d5e12896bac2b163da61c11612f91",
+    "url": "/static/media/project-tn-01.3c9d5e12.png"
+  },
+  {
+    "revision": "0a86fafb7ad22a2642ccf83ac2cccdd7",
+    "url": "/static/media/project-tn-02.0a86fafb.png"
+  },
+  {
+    "revision": "591c3fa908188eb83434f6174b747fea",
+    "url": "/static/media/project-tn-03.591c3fa9.png"
+  },
+  {
+    "revision": "043d7f8d8d7cddc0422a8ca7de0139ed",
+    "url": "/static/media/project-tn-04.043d7f8d.png"
+  },
+  {
+    "revision": "bb2a5f4555642603a5f873b806bd7126",
+    "url": "/static/media/prismvideo-img01.bb2a5f45.png"
+  },
+  {
+    "revision": "a4dedd123ccee7d8d63c9e60bdd4c644",
+    "url": "/static/media/prismvideo-img03.a4dedd12.png"
+  },
+  {
+    "revision": "7e75936e9230b479777e8c6e3eaf3684",
+    "url": "/static/media/prismvideo-img05.7e75936e.png"
+  },
+  {
+    "revision": "494f361e3762d8ac2b4435ef4e873c15",
+    "url": "/static/media/prismvideo-img06.494f361e.png"
+  },
+  {
+    "revision": "f509b31eb9ffafe2278a37f8a7fc00ea",
+    "url": "/static/media/prismvideo-img07.f509b31e.png"
+  },
+  {
+    "revision": "bb3f7d8c76bc2792989effe0219a4812",
+    "url": "/static/media/prismvideo-img08.bb3f7d8c.png"
+  },
+  {
+    "revision": "5790cb6f974622711fa7c3611e4d447d",
+    "url": "/static/media/prismvideo-img09.5790cb6f.png"
+  },
+  {
+    "revision": "09cd1e9a236963e57f5e19d13bc9693d",
+    "url": "/static/media/sklayout-img01.09cd1e9a.png"
+  },
+  {
+    "revision": "39bb86a71e5265d025618a907f44420d",
+    "url": "/static/media/prismlayout-img02.39bb86a7.png"
+  },
+  {
+    "revision": "801f7d7ac8100d4bb2b285b6d5f90223",
+    "url": "/static/media/prismlayout-img06.801f7d7a.png"
+  },
+  {
+    "revision": "644c21737ed3d95a191d0f5ca8a0134d",
+    "url": "/static/media/prismlayout-img07.644c2173.png"
+  },
+  {
+    "revision": "ff4873124db10161ae95",
+    "url": "/static/js/main.24649fdb.chunk.js"
+  },
+  {
+    "revision": "7a8d9aad1e35714f13a44fca8f4ca5ef",
+    "url": "/static/media/schole-img01.7a8d9aad.png"
+  },
+  {
+    "revision": "f558f8edca18e5c03ffdc92881a044bc",
+    "url": "/static/media/schole-img02.f558f8ed.png"
+  },
+  {
+    "revision": "04e068d19b19d39ca5c5649becc81ecf",
+    "url": "/static/media/schole-img03.04e068d1.png"
+  },
+  {
+    "revision": "b97ff6ac393c5959987c8cf78389d4da",
+    "url": "/static/media/schole-img04.b97ff6ac.png"
+  },
+  {
+    "revision": "9a6682153df6c38afd0ee9d5641cecb9",
+    "url": "/static/media/schole-img05.9a668215.png"
+  },
+  {
+    "revision": "0371ce0b6b01f5bdc270962a80b8decd",
+    "url": "/static/media/schole-img06.0371ce0b.png"
+  },
+  {
+    "revision": "99d5e973cd20f2f83356f155821a9b7c",
+    "url": "/static/media/schole-img07.99d5e973.png"
+  },
+  {
+    "revision": "1ace9cd7914a3bd4b24cae102ea4e6a5",
+    "url": "/static/media/schole-img08.1ace9cd7.png"
+  },
+  {
+    "revision": "831607757d22b9423b44d4318dd6376b",
+    "url": "/static/media/framer-img01.83160775.png"
+  },
+  {
+    "revision": "85711348266303e31b9f3c128a7d84b3",
+    "url": "/static/media/framer-img02.85711348.png"
+  },
+  {
+    "revision": "7f58c09f980bdd8beca49df35bc0dd48",
+    "url": "/static/media/framer-img03.7f58c09f.gif"
+  },
+  {
+    "revision": "fc230a83423830f2529915d1b801cea3",
+    "url": "/static/media/framer-img04.fc230a83.gif"
+  },
+  {
+    "revision": "f74cbf8c8125972347eda84b07237641",
+    "url": "/static/media/framer-img05.f74cbf8c.gif"
+  },
+  {
+    "revision": "2493f98f2a9e05e24b55a60f136578c9",
+    "url": "/static/media/framer-img06.2493f98f.gif"
+  },
+  {
+    "revision": "ba51f3af2f2503869c5489c5cdd85a94",
+    "url": "/static/media/framer-img07.ba51f3af.gif"
+  },
+  {
+    "revision": "37db9ed830150908c4c79ea501a0993f",
+    "url": "/static/media/article-tn-01.37db9ed8.png"
+  },
+  {
+    "revision": "d86a77d36bc3256733f63dbebecd0bda",
+    "url": "/static/media/article-tn-02.d86a77d3.png"
+  },
+  {
+    "revision": "d86a77d36bc3256733f63dbebecd0bda",
+    "url": "/static/media/article-tn-03.d86a77d3.png"
+  },
+  {
+    "revision": "ac196e03d44d3a8cdf73644603f81110",
+    "url": "/static/media/article-tn-04.ac196e03.png"
+  },
+  {
+    "revision": "31cdaaaf074bb78a9d74322edd63dbe3",
+    "url": "/static/media/article-tn-05.31cdaaaf.png"
+  },
+  {
+    "revision": "afd76b7436f53d6c521e29a9013b2663",
+    "url": "/static/media/article-tn-06.afd76b74.png"
+  },
+  {
+    "revision": "1afd1c21227fac3959a419e823513554",
+    "url": "/static/media/profile.1afd1c21.jpg"
+  },
+  {
+    "revision": "c7eead352c41f9cb37bac8996787b29f",
+    "url": "/static/media/Luis_SanjuanCV.c7eead35.pdf"
+  },
+  {
+    "revision": "9dd4203f5af96a07b048f63b39e8fe35",
+    "url": "/static/media/link-icon.9dd4203f.svg"
+  },
+  {
+    "revision": "3a41a9c8c1d8149910a459ba5fc69839",
+    "url": "/static/media/github-logo.3a41a9c8.png"
+  },
+  {
+    "revision": "861dfd272340380982ce705784d7b531",
+    "url": "/index.html"
+  }
+];
